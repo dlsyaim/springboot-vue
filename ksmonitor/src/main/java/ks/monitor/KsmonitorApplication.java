01f31e8172ebc9142c866c0edb5d3f6bef9f2463
dlsyaim <dlsyaim@gmail.com>
2018-03-12 10:03:08 +0800

    spring boot 搭建

---
+++ ksmonitor/src/main/java/ks/monitor/KsmonitorApplication.java
@@ -0,0 +1,12 @@
+package ks.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsmonitorApplication.class, args);
+	}
+}
---
+++ ksmonitor/src/test/java/ks/monitor/KsmonitorApplicationTests.java
@@ -0,0 +1,16 @@
+package ks.monitor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KsmonitorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
